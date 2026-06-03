60d4ce7a3245720329ed3be3ef472f2bf79c88d2
AdenArya211 <adenaryawardana@gmail.com>
2026-06-03 17:24:59 +0700

    Fix product search suspense

--- app/product/page.jsx
+++ app/product/page.jsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import Navbar from "../../components/Navbar";
 import ProductList from "../../components/ProductList";
 import Footer from "../../components/Footer";
@@ -6,7 +7,11 @@ export default function ProductPage() {
   return (
     <main className="min-h-screen bg-[#d9d9d9]">
       <Navbar />
-      <ProductList />
+
+      <Suspense fallback={<p>Loading products...</p>}>
+        <ProductList />
+      </Suspense>
+
       <Footer />
     </main>
   );
